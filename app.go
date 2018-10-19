2ed1cac1bb9c8b2552ef440de76ae3cede4ace7f
kurian <kurianck.mail@gmail.com>
2018-10-19 20:15:58 +0530

    story bot
        make the story bot, that will write stories by itself.

---
+++ app.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+   this application write stories.
+*/
+func main() {
+	/*
+		get the first letter
+		make a google reques
+		get the next letter
+		if number of words is greater than
+		word limit, exit the program
+	*/
+}
