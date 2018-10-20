b2472860a4ff4c744e2112e3b0c44147ec9445b5
kurian <kurianck.mail@gmail.com>
2018-10-20 06:51:49 +0530

    story teller
        making a story teller application.

--- app.go
+++ app.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"reflect"
+)
+
 /*
    this application write stories.
 */
@@ -11,4 +20,29 @@ func main() {
 		if number of words is greater than
 		word limit, exit the program
 	*/
+	var keyword string
+	clientUrl := os.Getenv("CLI")
+	fmt.Scanf("%s", &keyword)
+	r, err := http.Get(clientUrl + keyword)
+	if err != nil {
+		panic(err)
+	}
+	bytestring, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	var wordMap []interface{}
+	err = json.Unmarshal([]byte(bytestring), &wordMap)
+	if err != nil {
+		panic(err)
+	}
+	for _, val := range wordMap {
+		if rt := reflect.TypeOf(val); rt.Kind() == reflect.Slice {
+			for _, val1 := range val.([]interface{}) {
+				fmt.Println(val1)
+			}
+
+		}
+	}
+	return
 }
